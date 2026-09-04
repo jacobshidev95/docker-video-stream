132d16bb6b32010f70826355156fb5cabceef84d
jacobshidev95 <jacobshidev@gmail.com>
2026-09-04 09:12:58 +0800

    use aiortc to replace nicegui demo

--- main.py
+++ main.py
@@ -1,113 +1,178 @@
-#!/usr/bin/env python3
-import base64
-import signal
-import pyautogui
-import cv2
-import numpy as np
-from fastapi import Response
+import argparse
+import asyncio
+import json
+import logging
+import os
+import platform
+import ssl
+from typing import Optional
 
-from nicegui import Client, app, core, run, ui
+from aiohttp import web
+from aiortc import (
+    MediaStreamTrack,
+    RTCPeerConnection,
+    RTCRtpSender,
+    RTCSessionDescription,
+)
+from aiortc.contrib.media import MediaPlayer, MediaRelay
 
-@ui.page("/")
-def home():
-    ui.label("Video Stream")
-    ui.label("The application is running.")
+ROOT = os.path.dirname(__file__)
 
-# In case you don't have a webcam, this will provide a black placeholder image.
-black_1px = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII='
-placeholder = Response(content=base64.b64decode(black_1px.encode('ascii')), media_type='image/png')
-
-def convert(frame: np.ndarray) -> bytes:
-    """Converts a frame from OpenCV to a JPEG image.
-
-    This is a free function (not in a class or inner-function),
-    to allow run.cpu_bound to pickle it and send it to a separate process.
-    """
-    _, imencode_image = cv2.imencode('.jpg', frame)
-    return imencode_image.tobytes()
+pcs = set()
+relay = None
+webcam = None
 
 
-def setup() -> None:
-    # OpenCV is used to access the webcam.
-    video_capture = cv2.VideoCapture(0)  # 0--main web camera  1---second web camera
-    if video_capture.isOpened():
-        @app.get('/video/frame')
-        # Thanks to FastAPI's `app.get` it is easy to create a web route which always provides the latest image from OpenCV.
-        async def grab_video_frame() -> Response:
-            # display width & height
-            if not video_capture.isOpened():
-                return placeholder
-            # The `video_capture.read` call is a blocking function.
-            # So we run it in a separate thread (default executor) to avoid blocking the event loop.
-            frame = video_capture.read()[1]
+def create_local_tracks(
+    play_from: str, decode: bool
+) -> tuple[Optional[MediaStreamTrack], Optional[MediaStreamTrack]]:
+    global relay, webcam
 
-            if frame is None:
-                return placeholder
-            else:
-                # `convert` is a CPU-intensive function, so we run it in a separate process to avoid blocking the event loop and GIL.
-                jpeg = await run.cpu_bound(convert, frame)
-                return Response(content=jpeg, media_type='image/jpeg')
-
-        @ui.page('/')
-        def page():
-            # For non-flickering image updates and automatic bandwidth adaptation an interactive image is much better than `ui.image()`.
-            # 2. GET the current frame size
-            width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH) if cv2.__version__.startswith(
-                '3') else video_capture.get(
-                cv2.CAP_PROP_FRAME_WIDTH)
-            height = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            print(f"Camera Resolution: {int(width)}x{int(height)}")
-            # get screen size
-            # Returns a named tuple like Size(width=1920, height=1080)
-            screen_width, screen_height = pyautogui.size()
-            print(f"Screen Resolution: {int(screen_width)}x{int(screen_height)}")
-            if int(width) < screen_width: # & int(height) < screen_height:
-                display_width = int(width)
-                display_height = int(height)
-                print("int(width) < screen_width & int(height) < screen_height:")
-            else:
-                display_width = screen_width
-                display_height = screen_height
-                print("int(width) > screen_width & int(height) > screen_height:")
-            print(f"Display Resolution: {int(display_width)}x{int(display_height)}")
-            size_style = 'width: ' + str(display_width) + 'px; height: ' + str(display_height) + 'px'
-            video_image = ui.interactive_image('/video/frame').style(size_style)
-
-            # A timer constantly updates the source of the image.
-            ui.timer(interval=0.05, callback=video_image.force_reload)
-
-        async def disconnect() -> None:
-            """Disconnect all clients from current running server."""
-            for client_id in Client.instances:
-                await core.sio.disconnect(client_id)
-
-        def handle_sigint(signum, frame) -> None:
-            # `disconnect` is async, so it must be called from the event loop; we use `ui.timer` to do so.
-            ui.timer(0.1, disconnect, once=True)
-            # Delay the default handler to allow the disconnect to complete.
-            ui.timer(1, lambda: signal.default_int_handler(signum, frame), once=True)
-
-        async def cleanup() -> None:
-            # This prevents ugly stack traces when auto-reloading on code change,
-            # because otherwise disconnected clients try to reconnect to the newly started server.
-            await disconnect()
-            # Release the webcam hardware so it can be used by other applications again.
-            video_capture.release()
-
-        # app.on_shutdown(cleanup)
-        # We also need to disconnect clients when the app is stopped with Ctrl+C,
-        # because otherwise they will keep requesting images which lead to unfinished subprocesses blocking the shutdown.
-        signal.signal(signal.SIGINT, handle_sigint)
+    if play_from:
+        # If a file name was given, play from that file.
+        player = MediaPlayer(play_from, decode=decode)
+        return player.audio, player.video
     else:
-        print("No video capture device")
-        # app.on_shutdown()
+        # Otherwise, play from the system's default webcam.
+        #
+        # In order to serve the same webcam to multiple users we make use of
+        # a `MediaRelay`. The webcam will stay open, so it is our responsability
+        # to stop the webcam when the application shuts down in `on_shutdown`.
+        options = {"framerate": "30", "video_size": "640x480"}
+        if relay is None:
+            if platform.system() == "Darwin":
+                webcam = MediaPlayer(
+                    "default:none", format="avfoundation", options=options
+                )
+            elif platform.system() == "Windows":
+                webcam = MediaPlayer(
+                    "video=Integrated Camera", format="dshow", options=options
+                )
+            else:
+                webcam = MediaPlayer("/dev/video0", format="v4l2", options=options)
+            relay = MediaRelay()
+        return None, relay.subscribe(webcam.video)
 
 
-# All the setup is only done when the server starts. This avoids the webcam being accessed
-# by the auto-reload main process (see https://github.com/zauberzeug/nicegui/discussions/2321).
-#app.on_startup(setup)
+def force_codec(pc: RTCPeerConnection, sender: RTCRtpSender, forced_codec: str) -> None:
+    kind = forced_codec.split("/")[0]
+    codecs = RTCRtpSender.getCapabilities(kind).codecs
+    transceiver = next(t for t in pc.getTransceivers() if t.sender == sender)
+    transceiver.setCodecPreferences(
+        [codec for codec in codecs if codec.mimeType == forced_codec]
+    )
 
-if __name__ in {"__main__", "__mp_main__"}:
-    app.on_startup(setup)
-    ui.run(host='0.0.0.0', port=8090, reload=True)
 
+async def index(request: web.Request) -> web.Response:
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def javascript(request: web.Request) -> web.Response:
+    content = open(os.path.join(ROOT, "client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
+
+
+async def offer(request: web.Request) -> web.Response:
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange() -> None:
+        print("Connection state is %s" % pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    # open media source
+    audio, video = create_local_tracks(
+        args.play_from, decode=not args.play_without_decoding
+    )
+
+    if audio:
+        audio_sender = pc.addTrack(audio)
+        if args.audio_codec:
+            force_codec(pc, audio_sender, args.audio_codec)
+        elif args.play_without_decoding:
+            raise Exception("You must specify the audio codec using --audio-codec")
+
+    if video:
+        video_sender = pc.addTrack(video)
+        if args.video_codec:
+            force_codec(pc, video_sender, args.video_codec)
+        elif args.play_without_decoding:
+            raise Exception("You must specify the video codec using --video-codec")
+
+    await pc.setRemoteDescription(offer)
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
+
+async def on_shutdown(app: web.Application) -> None:
+    # Close peer connections.
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    # pcs.clear()
+
+    # If a shared webcam was opened, stop it.
+    if webcam is not None:
+        webcam.video.stop()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument("--play-from", help="Read the media from a file and sent it.")
+    parser.add_argument(
+        "--play-without-decoding",
+        help=(
+            "Read the media without decoding it (experimental). "
+            "For now it only works with an MPEGTS container with only H.264 video."
+        ),
+        action="store_true",
+    )
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument("--verbose", "-v", action="count")
+    parser.add_argument(
+        "--audio-codec", help="Force a specific audio codec (e.g. audio/opus)"
+    )
+    parser.add_argument(
+        "--video-codec", help="Force a specific video codec (e.g. video/H264)"
+    )
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/client.js", javascript)
+    app.router.add_post("/offer", offer)
+    web.run_app(app, host=args.host, port=args.port, ssl_context=ssl_context)
